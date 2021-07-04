47da1aacf6c19dcf3d35f1a481a590af3a84dd02
荏狸十三 <renshisan@qq.com>
2021-07-04 22:02:29 +0800

    初始化项目：安装"React"和"React-Dom"依赖

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,3 +1,4 @@
-const div = document.createElement('div')
-div.innerText='Hi，Nico-Ui 我们很期待你的版本发布！'
-document.body.appendChild(div)
+import React from 'react'
+import ReactDOM from 'react-dom'
+console.log(React)
+console.log(ReactDOM)
